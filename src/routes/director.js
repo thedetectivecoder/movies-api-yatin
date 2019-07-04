2249089f37013ffdd5f00465cc1b72eaa6f5cc9c
Yatin Burhmi <yatinburhmi@gmail.com>
2019-07-04 10:16:43 +0530

    Completes error handling

--- src/routes/director.js
+++ src/routes/director.js
@@ -4,40 +4,67 @@ const dir = require('../models/director.js');
 const router = express.Router();
 const validate = require('../utils/validation');
 
-router.get('/', async (req, res) => {
-  const arrDirector = await dir.getDirector();
-  res.send(arrDirector);
-});
-
-router.get('/:id', async (req, res) => {
-  // res.send(req.params.id);
-  const dirName = await dir.selectDirectorById(req.params.id);
-  res.send(dirName);
-});
-
-router.post('/', async (req, res) => {
-  const validRes = validate.directorValidation(req.body);
-  if (validRes.error) {
-    res.status(400).send(`${validRes.error.name} : ${validRes.error.details[0].message}`);
-  } else {
-    const directorAdd = await dir.addDirector(req.body);
-    res.send(directorAdd);
+router.get('/', async (req, res, next) => {
+  try {
+    const arrDirector = await dir.getDirector();
+    res.send(arrDirector);
+  } catch (err) {
+    next(err);
   }
 });
 
-router.put('/:id', async (req, res) => {
-  const validRes = validate.directorValidation(req.body);
-  if (validRes.error) {
-    res.status(400).send(`${validRes.error.name} : ${validRes.error.details[0].message}`);
-  } else {
-    const updateDir = dir.updateDirector(req.params.id, req.body);
-    res.send(updateDir);
+router.get('/:id', async (req, res, next) => {
+  try {
+    const validRes = validate.idValidation(req.params.id);
+    if (validRes.error) {
+      res.status(400).send(`${validRes.error.name} : ${validRes.error.details[0].message}`);
+    } else {
+      const dirName = await dir.selectDirectorById(req.params.id);
+      res.send(dirName);
+    }
+  } catch (err) {
+    next(err);
   }
 });
 
-router.delete('/:id', async (req, res) => {
-  const delDir = await dir.deleteDirector(req.params.id);
-  res.send(delDir);
+router.post('/', async (req, res, next) => {
+  try {
+    const validRes = validate.directorValidation(req.body);
+    if (validRes.error) {
+      res.status(400).send(`${validRes.error.name} : ${validRes.error.details[0].message}`);
+    } else {
+      const directorAdd = await dir.addDirector(req.body);
+      res.send(directorAdd);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const idValidRes = validate.idValidation(req.params.id);
+    const validRes = validate.directorValidation(req.body);
+    if (validRes.error) {
+      res.status(400).send(`${validRes.error.name} : ${validRes.error.details[0].message}`);
+    } else if (idValidRes.error) {
+      res.status(400).send(`${idValidRes.error.name} : ${idValidRes.error.details[0].message}`);
+    } else {
+      const updateDir = dir.updateDirector(req.params.id, req.body);
+      res.send(updateDir);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const delDir = await dir.deleteDirector(req.params.id);
+    res.send(delDir);
+  } catch (err) {
+    next(err);
+  }
 });
 
 
--- src/routes/movies.js
+++ src/routes/movies.js
@@ -4,42 +4,74 @@ const dir = require('../models/movie');
 const router = express.Router();
 const validate = require('../utils/validation');
 
-router.get('/', async (req, res) => {
-  const arrMovie = await dir.getMovies();
-  res.send(arrMovie);
-});
-
-router.get('/:id', async (req, res) => {
-  const movName = await dir.selectMovieById(req.params.id);
-  res.send(movName);
-});
-
-router.post('/', async (req, res) => {
-  const validRes = validate.addMovieValidation(req.body);
-  if (validRes.error) {
-    res.status(400).send(`${validRes.error.name} : ${validRes.error.details[0].message}`);
-  } else {
-    const addMovieObj = await dir.addMovie(req.body);
-    res.send(addMovieObj);
+router.get('/', async (req, res, next) => {
+  try {
+    const arrMovie = await dir.getMovies();
+    res.send(arrMovie);
+  } catch (err) {
+    next(err);
   }
 });
 
-router.put('/:id', async (req, res) => {
-  const validRes = validate.updateMovieValidation(req.body);
-  if (validRes.error) {
-    res.status(400).send(`${validRes.error.name} : ${validRes.error.details[0].message}`);
-  } else {
-    const updateMovieObj = await dir.updateMovie(req.params.id, req.body);
-    res.send(updateMovieObj);
+router.get('/:id', async (req, res, next) => {
+  try {
+    const validRes = validate.idValidation(req.params.id);
+    if (validRes.error) {
+      res.status(400).send(`${validRes.error.name} : ${validRes.error.details[0].message}`);
+    } else {
+      const movName = await dir.selectMovieById(req.params.id);
+      res.send(movName);
+    }
+  } catch (err) {
+    next(err);
   }
 });
 
-router.delete('/:id', async (req, res) => {
-  const movName = await dir.deleteMovie(req.params.id);
-  res.send(movName);
+router.post('/', async (req, res) , next=> {
+  try{
+    const validRes = validate.addMovieValidation(req.body);
+    if (validRes.error) {
+      res.status(400).send(`${validRes.error.name} : ${validRes.error.details[0].message}`);
+    } else {
+        const addMovieObj = await dir.addMovie(req.body);
+        res.send(addMovieObj);
+    }
+  } catch(err) {
+      next(err);
+  }
+});
+
+router.put('/:id', async (req, res, next) => {
+  try{
+    const idValidRes = validate.idValidation(req.params.id);
+    const validRes = validate.updateMovieValidation(req.body);
+    if (validRes.error) {
+      res.status(400).send(`${validRes.error.name} : ${validRes.error.details[0].message}`);
+    } else if(idValidRes.error){
+      res.status(400).send(`${idValidRes.error.name} : ${idValidRes.error.details[0].message}`);
+    } else {
+        const updateMovieObj = await dir.updateMovie(req.params.id, req.body);
+        res.send(updateMovieObj);
+    }
+  } catch(err){
+      next(err);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try{
+    const validRes = validate.idValidation(req.params.id);
+    if(validRes.error){
+      res.status(400).send(`${validRes.error.name} : ${validRes.error.details[0].message}`);
+    } else{
+      const movName = await dir.deleteMovie(req.params.id);
+      res.send(movName);
+    }
+  } catch(err){
+      next(err);
+  }
 });
 
 // app.listen(3000, () => console.log('Server Starting....'));
-
 
 module.exports = router;
